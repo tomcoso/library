d7612b6e34b84b28c49da3a1b71ab349f84f6fd9
tomcoso <tomasdessy0@gmail.com>
2022-06-29 19:33:20 +0100

    start planning code structure

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 // Tomas Dessy
 
-let library = [];
+let library = [
+    star5 = [],
+    star4 = [],
+    star3 = [],
+    star2 = [],
+    star1 = [],
+];
 
 function Book(title, author, status) {
 
@@ -14,7 +20,34 @@ function addToLibrary(title, author, status) {
 
     const newBook = new Book(title, author, status);
 
-    library.push(newBook);
+    library[star3].push(newBook);
     console.log(library);
 
 }
+
+Book.prototype.changeRating = function(book, target) {
+    
+}
+
+
+const form = document.querySelector('#book-form');
+const container = document.querySelector('.container');
+
+function displayForm() {
+    if (form.classList.contains("hidden"))
+    {
+        form.classList.remove('hidden');
+        container.classList.add('dark');
+    }
+    else
+    {
+        form.classList.add('hidden');
+        container.classList.remove('dark');
+    }
+}
+
+const showFormBtn = document.querySelector('.display-form');
+showFormBtn.addEventListener('click', displayForm)
+
+const hideFormBtn = document.querySelector('#cancel-btn');
+hideFormBtn.addEventListener('click', displayForm)
