765b2c6f7ddd775d585dcbf1934e438543b14b9e
tomcoso <tomasdessy0@gmail.com>
2022-06-30 14:13:05 +0100

    code core functions

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # library
 Library project from TOP
+
+I started coding the UI and at the time of coding the script I realised I was a bit ambitious. But I will still do my best to get everything done. It shouldnt be too hard.
+
+The biggest problem will be interacting with the DOM, since I need a way to move books on the DOM by dragging elements, and then sort them in different places by changing the book's object properties.
--- script.js
+++ script.js
@@ -1,32 +1,96 @@
 // Tomas Dessy
 
-let library = [
-    star5 = [],
-    star4 = [],
-    star3 = [],
-    star2 = [],
-    star1 = [],
-];
+// Book object constructor and a function that generates new books
+// v
+// Array of books (how to name each book object?)
+// v
+// Place in the dom according to specific properties (rating)
 
-function Book(title, author, status) {
+let library = [];
+
+
+function Book(title, author, status, rating, color) {
 
     this.title = title;
     this.author = author;
     this.status = status;
+    this.rating = rating;
+    this.color = color
 
+};
+
+
+function updateLibrary() {
+
+    const shelf = document.querySelectorAll('.books');
+
+    for(let j in shelf) {
+        // console.log(shelf[j]);
+        while (shelf[j].firstChild) {
+            // console.log(shelf[j].firstChild);
+            shelf[j].removeChild(shelf[j].firstChild);
+        }
+
+    }
+
+    for (let i in library) {
+
+        for (let j in shelf) {
+
+            // console.log(library[i].rating, shelf[j].attributes[1].value);
+            if (library[i].rating === shelf[j].attributes[1].value) {
+
+                shelf[j].append(library[i].createElement());
+                break
+            }
+
+        }
+    };
 }
 
-function addToLibrary(title, author, status) {
 
-    const newBook = new Book(title, author, status);
+function addToLibrary(title, author, status, rating = 3, color = 'peru') {
 
-    library[star3].push(newBook);
+    const newBook = new Book(title, author, status, rating, color);
+
+    library.push(newBook);
+
+    const bookElement = document.createElement('div');
+    bookElement.classList.add('book');
+    bookElement.setAttribute('data-title', title);
+    bookElement.setAttribute('style', `content-visibility: hidden; background-color: ${color};`);
+    bookElement.textContent = `${title}<br>${author}<br>${status}`;
+
+    updateLibrary(bookElement);
     console.log(library);
 
+};
+
+Book.prototype.createElement = function() {
+
+    const bookElement = document.createElement('div');
+    bookElement.classList.add('book');
+    bookElement.setAttribute('data-title', this.title);
+    bookElement.setAttribute('style', `content-visibility: hidden; background-color: ${this.color};`);
+    bookElement.textContent = `${this.title}<br>${this.author}<br>${this.status}`;
+
+    return bookElement;
 }
 
-Book.prototype.changeRating = function(book, target) {
+Book.prototype.changeRating = function(newRating) {
+
+    this.rating = newRating;
     
+    updateLibrary();
+    return `${this.title} is now ${this.rating} stars`
+};
+
+Book.prototype.delete = function() {
+
+    library.splice(library.indexOf(this), 1);
+
+    updateLibrary();
+    return `${this.title} deleted`;
 }
 
 
@@ -44,10 +108,10 @@ function displayForm() {
         form.classList.add('hidden');
         container.classList.remove('dark');
     }
-}
+};
 
 const showFormBtn = document.querySelector('.display-form');
-showFormBtn.addEventListener('click', displayForm)
+showFormBtn.addEventListener('click', displayForm);
 
 const hideFormBtn = document.querySelector('#cancel-btn');
-hideFormBtn.addEventListener('click', displayForm)
+hideFormBtn.addEventListener('click', displayForm);
